01283ccc22e87c3de91cab758605fc17ec80f71d
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-13 21:57:53 -0300

    feat: config mongodb

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(express.json());
 dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+try {
+    await client.connect();
+    console.log("MongoDB conectado");
+} catch (error) {
+    console.log(error);
+}
 
 app.listen(process.env.PORT, () => console.log(`Server running in port ${process.env.PORT}`));
